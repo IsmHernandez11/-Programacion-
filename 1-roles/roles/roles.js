f96ca2f947601c5421717d127f4eb8d4d204564e
IsmHernandez11 <ismasnayhernandez2010@gmail.com>
2026-02-18 20:05:40 -0500

    se crearon funciones en roles js

---
+++ 1-roles/roles/roles.js
@@ -0,0 +1,60 @@
+let empleados = [
+    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+]
+let roles = [];
+
+mostrarEmpleados = function () {
+    let divTabla = document.getElementById("tablaEmpleados");
+
+    let contenido = "<table border='1'>";
+    contenido += "<tr>";
+    contenido += "<th>CEDULA</th>";
+    contenido += "<th>NOMBRE</th>";
+    contenido += "<th>APELLIDO</th>";
+    contenido += "<th>SUELDO</th>";
+    contenido += "</tr>";
+
+    for (let i = 0; i < empleados.length; i++) {
+        let emp = empleados[i];
+        contenido += "<tr>";
+        contenido += "<td>" + emp.cedula + "</td>";
+        contenido += "<td>" + emp.nombre + "</td>";
+        contenido += "<td>" + emp.apellido + "</td>";
+        contenido += "<td>" + emp.sueldo + "</td>";
+        contenido += "</tr>";
+    }
+
+    contenido += "</table>";
+    divTabla.innerHTML = contenido;
+}
+
+mostrarOpcionEmpleado = function () {
+    mostrarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    ocultarComponente("divResumen");
+
+    deshabilitarFormularioEmpleado();
+    mostrarEmpleados();
+}
+
+
+mostrarOpcionRol = function () {
+    ocultarComponente("divEmpleado");
+    mostrarComponente("divRol");
+    ocultarComponente("divResumen");
+
+    deshabilitarComponente("btnGuardarRol");
+}
+
+
+
+mostrarOpcionResumen = function () {
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    mostrarComponente("divResumen");
+
+    mostrarRoles();
+    mostrarTotales();
+}
+
